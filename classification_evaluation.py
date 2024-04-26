822263851f426bd64c0272f6602106084a77cdfa
Winterblazerx <71366537+Winterblazerx@users.noreply.github.com>
2024-04-25 17:43:07 -0700

    asd

--- classification_evaluation.py
+++ classification_evaluation.py
@@ -19,8 +19,23 @@ NUM_CLASSES = len(my_bidict)
 # And get the predicted label, which is a tensor of shape (batch_size,)
 # Begin of your code
 def get_label(model, model_input, device):
-    answer = model(model_input, device)
-    return answer
+    batch_size = model_input.shape[0]
+    log_probs = torch.zeros((batch_size, NUM_CLASSES)).to(device)
+
+    # Get loss of each label for the input
+    for label in range(NUM_CLASSES):
+        labels = torch.full((batch_size,), label).to(device)
+        with torch.no_grad():
+            answer = model(model_input, labels, sample=False)
+
+            # Adding False statement to get loss per batch
+            loss = discretized_mix_logistic_loss(model_input, answer, training=False)
+            log_probs[:, label] = loss
+
+    # Find the label with minimum loss for each sample
+    _, min_labels = torch.min(log_probs, dim=1)
+
+    return min_labels
 # End of your code
 
 def classifier(model, data_loader, device):
@@ -64,7 +79,8 @@ if __name__ == '__main__':
     #Write your code here
     #You should replace the random classifier with your trained model
     #Begin of your code
-    model = random_classifier(NUM_CLASSES)
+    PATH = "models/conditional_pixelcnn.pth"
+    model = PixelCNN(nr_resnet=1, nr_filters=80, input_channels=3, nr_logistic_mix=5)
     #End of your code
     
     model = model.to(device)
